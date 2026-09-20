5b1be4a192d254a3269666e07eae91faa52f2d83
zagzy8776 <127265311+zagzy8776@users.noreply.github.com>
2026-09-21 00:58:19 +0200

    Fix TanStack error component unknown error type

--- app/src/routes/__root.tsx
+++ app/src/routes/__root.tsx
@@ -81,7 +81,7 @@ function NotFoundComponent() {
   );
 }
 
-function ErrorComponent({ error, reset }: { error: Error; reset: () => void }) {
+function ErrorComponent({ error, reset }: { error: unknown; reset: () => void }) {
   console.error(error);
   const router = useRouter();
   useEffect(() => {
